0aff0bcce36a56dc7db5826648a3381c4775f6e9
Simon Rönnberg <seamonr@gmail.com>
2018-03-13 21:33:02 +0100

    feat: Add pick ray calculation

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub extern crate rhusics_core;
 pub extern crate rhusics_ecs;
 
 extern crate amethyst_core;
+extern crate amethyst_renderer;
 extern crate shrev;
 extern crate specs;
 
@@ -15,10 +16,12 @@ pub use self::bundle::{BasicPhysicsBundle2, BasicPhysicsBundle3, SpatialPhysicsB
 pub use self::default::{DefaultBasicPhysicsBundle2, DefaultBasicPhysicsBundle3,
                         DefaultSpatialPhysicsBundle2, DefaultSpatialPhysicsBundle3,
                         PoseTransformSyncSystem2, PoseTransformSyncSystem3};
+pub use self::pick::{pick_ray, pick_ray_ndc};
 pub use self::sync::{time_sync, AsTransform, Convert};
 pub use self::system::PoseTransformSyncSystem;
 
 mod bundle;
 mod default;
+mod pick;
 mod sync;
 mod system;
